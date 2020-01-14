672b42227e92f2fc440ad52d4bb1f8f374b7fe5d
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-01-14 12:32:10 -0700

    Create and connect initial components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <PoD msg="App" v-if="this.show==='today' @selectMonth="selectMonth" />
+    <MonthlyPoD v-if="this.show==='month'" @selectToday="selectToday" />
+    <CurrentPoD v-if="this.show==='current' />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PoD from './components/PoD.vue'
+import CurrentPoD from './components/CurrentPoD.vue'
+import MonthlyPoD from './components/MonthlyPoD'
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    PoD,
+    MonthlyPoD,
+    CurrentPod
+  },
+  data() {
+    return {
+      currentPoD: {},
+      todatPoD:{},
+      monthlyPoD:[],
+      show:'today'
+    }
+  },
+  methods:{
+    selectMonth(){
+      this.show = 'month'
+    },
+    selectToday(){
+      this.show='today'
+    }
+  },
+
 }
 </script>
 
 <style>
-#app {
+* {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+  box-sizing:border-box;
+}
+#app {
+  background-color: #AFAFAF;
+  height:100vh;
+  padding: 1%
 }
 </style>
---
+++ src/components/MonthlyPoD.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <header><button @click="selectMonth">Today's PoD</button></header>
+    <p>Month Month</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MonthlyPoD',
+  props: {
+    msg: String
+  },
+    methods:{
+    selectMonth(){
+      this.$emit('selectToday')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
