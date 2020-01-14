074cdcb5a66d872fac45928a54a545f8593279d7
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-01-14 14:56:43 -0700

    Add APICalls and fetch function

---
+++ utils/APIcalls.js
@@ -0,0 +1,10 @@
+const key = process.env.VUE_APP_API_KEY
+const baseUrl = `https://api.nasa.gov/planetary/apod?api_key=${key}`;
+
+export const getThisMonthPics = () => {
+  let date = `${new Date().getFullYear()}-${String(new Date().getMonth() + 1 ).padStart(2, '0')}-01}`;
+  
+  return fetch(`${baseUrl}&start_date=${date}`)
+    .then((res) => res.json())
+    .catch((error) => console.log(error));
+};
