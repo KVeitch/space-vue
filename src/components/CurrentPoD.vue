c71453c03a4974020d224b3ce4632dbf17af6009
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-01-14 12:32:10 -0700

    Create and connect initial components

---
+++ src/components/CurrentPoD.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <p>Current pod</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CurrentPoD',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/PoD.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="pod">
+    <header>
+      <button @click="selectMonth">Click</button>
+    </header>
+
+    <p>pod</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PoD',
+  props: {
+    msg: String,
+  },
+  methods:{
+    selectMonth(){
+      this.$emit('selectMonth')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ /* button: {
+   height:
+ } */
+</style>
