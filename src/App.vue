e294d66aa0e0e0d52cefcb8335d60202e3ed753a
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-01-14 19:36:47 -0700

    Improves styling and add selected images functionality

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <div v-if="!this.loaded" class="loading">
+      <img class="loading-image" alt="Planets orbiting the Sun" src="./assets/loading2.gif">
+    </div>
     <SinglePoD msg="App" v-if="this.show==='today' && this.loaded" @selectMonth="selectMonth" v-bind:pod="this.todayPoD" ></SinglePoD>
-    <MonthlyPoD msg="yep" v-if="this.show==='month'" @selectToday="selectToday" v-bind:monthly="this.monthlydata"></MonthlyPoD>
-    <SinglePoD v-if="this.show==='selected'" @selectMonth="selectMonth" ></SinglePoD>
+    <MonthlyPoD msg="yep" v-if="this.show==='month'" @selectToday="selectToday" @selectCurrent="selectCurrent" v-bind:monthly="this.monthlydata"></MonthlyPoD>
+    <SinglePoD v-if="this.show==='selected'" @selectMonth="selectMonth" v-bind:pod="this.selected"></SinglePoD>
   </div>
 </template>
 
 <script>
+
 import SinglePoD from './components/SinglePoD';
 import MonthlyPoD from './components/MonthlyPoD';
-import {getThisMonthPics} from '../utils/APIcalls';
+import { getThisMonthPics } from '../utils/APIcalls';
 
 export default {
   name: 'app',
@@ -34,7 +37,10 @@ export default {
     selectToday(){
       this.show='today'
     },
-    selectCurrent(){
+    selectCurrent(index){
+      console.log('Here==========>',index)
+      this.selected = this.monthlydata[index]
+      console.log(this.monthlydata[index])
       this.show='selected'
     },
   },
@@ -59,11 +65,25 @@ export default {
   padding: 0;
   box-sizing:border-box;
 }
-
+body{
+  background-color: #AFAFAF;
+}
 #app {
   background-color: #AFAFAF;
   height:100vh;
   padding: 10px
+}
+.loading{
+  height:100%;
+  width:100%;
+  background-color: black;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+}
+.loadingImage {
+  height:600px;
+  width:600px;
 }
 
 header{
